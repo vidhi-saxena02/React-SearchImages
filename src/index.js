e3e27797b6f6b9a02c0866a0d3dc9193097098d4
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-01-08 01:27:45 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+
+const element = document.getElementById("root");
+
+const root = ReactDom.createRoot(element);
+
+root.render(<App />);
